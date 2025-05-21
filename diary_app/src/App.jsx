46daec75265b3545d75c8a7f72002fc21f0fdf9a
Alexander <alexmenshikov7@gmail.com>
2025-05-21 17:10:41 +0500

    added new data for journal item

--- diary_app/src/App.jsx
+++ diary_app/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import JournalItem from './components/JournalItem/JournalItem';
 import './App.css';
-
 function App() {
-	const [count, setCount] = useState(0);
+
+	const data = [
+		{
+			text: 'yo',
+			date: new Date(),
+			title:'title'
+		},
+		{
+			text: 'yo',
+			date: new Date(),
+			title:'title'
+		}
+	];
+
 
 	return (
 		<>
-			<div>
-				<a href='https://vite.dev'  target='_blank' rel="noreferrer"> 
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://react.dev' target='_blank' rel="noreferrer">
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-				</button>
-				<p>
-          Edit <code>src/App.jsx</code> and save to test HMR 
-				</p>
-			</div>
-			<p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-			</p>
+			<JournalItem text={data[0].text} date={data[0].date} title={data[0].title}/>
 		</>
 	);
 }
