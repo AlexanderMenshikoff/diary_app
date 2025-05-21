bef042782192710309323cb41de2718685e2637c
Alexander <alexmenshikov7@gmail.com>
2025-05-21 17:11:04 +0500

    created brand new component journal item

---
+++ diary_app/src/components/JournalItem/JournalItem.jsx
@@ -0,0 +1,20 @@
+import './JournalItem.css';
+
+const JournalItem = ({title, date, text}) => {
+
+	const formatedDate = new Intl.DateTimeFormat('ru-Ru').format(date);
+
+	return(
+		<div className='journal_item'>
+			<h2 className='journal_item_header'>{title}</h2>
+			<div className='journal_item_body'>
+				<div className='journal_item_date'>{formatedDate}</div>
+				<div className='journal_item_text'>{text}</div>
+			</div>
+		</div>
+	);
+	
+};
+
+
+export default JournalItem;
