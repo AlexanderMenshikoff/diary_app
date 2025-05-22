748146a23b028ef015209cc5e26c8faec48e70d8
Alexander <alexmenshikov7@gmail.com>
2025-05-22 16:21:36 +0500

    added components into App, made composition

--- diary_app/src/App.jsx
+++ diary_app/src/App.jsx
@@ -1,5 +1,11 @@
 import JournalItem from './components/JournalItem/JournalItem';
 import './App.css';
+import CardButton from './components/CardButton/CardButton';
+import LeftPanel from './layout/LeftPanel/LeftPanel';
+import Body from './layout/Body/Body';
+import Header from './components/Header/Header';
+import JournalList from './components/JournalList/JournalList';
+import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 function App() {
 
 	const data = [
@@ -17,9 +23,24 @@ function App() {
 
 
 	return (
-		<>
-			<JournalItem text={data[0].text} date={data[0].date} title={data[0].title}/>
-		</>
+		<div className='app'>
+			<LeftPanel>
+				<Header/>
+				<JournalAddButton/>
+				<JournalList>
+					<CardButton>
+						<JournalItem text={data[0].text} date={data[0].date} title={data[0].title}/>
+					</CardButton>
+				</JournalList>
+			</LeftPanel>
+
+			<Body>
+				content
+			</Body>
+
+
+			
+		</div>
 	);
 }
 
