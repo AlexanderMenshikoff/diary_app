43ea23f0b96800bedbc5a13315d212ff202df43d
Alexander <alexmenshikov7@gmail.com>
2025-05-26 11:30:08 +0500

    get onSubmit function prop for proper data

--- diary_app/src/components/JournalForm/JournalForm.jsx
+++ diary_app/src/components/JournalForm/JournalForm.jsx
@@ -1,19 +1,22 @@
 import './JournalForm.css';
 import Button from '../Button/Button';
 
-const JournalForm = () => {
+
+const JournalForm = ({onSubmit}) => {
 
 	const formSubmit = (e) => {
 		e.preventDefault();
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
 		console.log(formProps);
+		onSubmit(formProps);
+		
 	};
 	return(
 		<form className='journal-form' onSubmit={formSubmit}>
-			<input type='text' name="text"/>
+			<input type='text' name="title"/>
 			<input type='date' name="date"/>
-			<textarea name="post" id="" cols="30" rows="10"></textarea>
+			<textarea name="text" id="" cols="30" rows="10"></textarea>
 			<Button text="Сохранить"/>
 		</form>
 	);
